1b97b742fd40d93e6333cf9168fbf36311df18f1
Meggazord <k_megahed@hotmail.com>
2024-05-21 19:42:20 +0300

    fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 First API App
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """Handle teardown of the app context."""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
