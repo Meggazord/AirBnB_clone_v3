b72caa8e846390ed096ad2dfeb0d1a031df8d78a
Meggazord <k_megahed@hotmail.com>
2024-05-21 19:37:48 +0300

    added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,17 @@ index file
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
+    """ Returns Status OK """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats()
+    """ Returns Stats """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
