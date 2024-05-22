8f859776bca726080a5672b27701a6d25469b359
Meggazord <k_megahed@hotmail.com>
2024-05-23 00:31:18 +0300

    fxied

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,14 @@ First API App
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exception):
